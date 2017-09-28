b05ccc006931cbe330d2d63e0aa76904aa4f5152
michellehodges <mischamangan@gmail.com>
2017-09-28 17:16:28 -0400

    Initial commit

--- src/main/java/com/theironyard/charlotte/sampleherokusite/BookController.java
+++ src/main/java/com/theironyard/charlotte/sampleherokusite/BookController.java
@@ -14,19 +14,30 @@ public class BookController {
     @CrossOrigin
     @RequestMapping(path = "/books", method = RequestMethod.GET)
     public List<Book> getBooks() {
-//        Book greenEggsAndHam = new Book("1234","Green Eggs And Ham",
-//                                        "Dr. Seuss!", true,
-//                            "Awesome", true, 67 );
-//        books.add(greenEggsAndHam);
+
         return books;
     }
-    // {"isbn":"12345","title":"The Great Gatsomething","author":"yamama"}
 
-//    @CrossOrigin(origins = {"http://thegoogs.com", "yahoo.com", "idk.com"})
+    //@CrossOrigin(origins = {"http://thegoogs.com", "yahoo.com", "idk.com"})
     @CrossOrigin
     @RequestMapping(path = "/books", method = RequestMethod.POST)
     public void addBook(@RequestBody Book book) {
+        //Book greenEggsAndHam = new Book("1234","Green Eggs And Ham",
+        //                        "Dr. Seuss!", true, "Awesome", true, 67 );
+        //books.add(greenEggsAndHam);
         books.add(book);
 
+    }
+
+    @CrossOrigin
+    @RequestMapping(path = "/checkin", method = RequestMethod.PATCH)
+    public void checkInBook(@RequestBody Book book) {
+        book.setCheckedOut(false);
+    }
+
+    @CrossOrigin
+    @RequestMapping(path = "/checkout", method = RequestMethod.PATCH)
+    public void checkOutBook(@RequestBody Book book) {
+        book.setCheckedOut(true);
     }
 }
